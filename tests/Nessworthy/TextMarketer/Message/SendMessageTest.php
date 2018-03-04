65e94d198c563c4e123e91c23e8af917219d5a7b
Sean Nessworthy <sean@nessworthy.me>
2018-03-04 00:44:29 +0000

    Adds more tests for SendMessage. Not the best but alright for now. Closes #5.

--- tests/Nessworthy/TextMarketer/Message/SendMessageTest.php
+++ tests/Nessworthy/TextMarketer/Message/SendMessageTest.php
@@ -2,6 +2,7 @@
 
 namespace Nessworthy\TextMarketer\Tests\Message;
 
+use Nessworthy\TextMarketer\Message\InvalidMessageException;
 use Nessworthy\TextMarketer\Message\SendMessage;
 use PHPUnit\Framework\TestCase;
 
@@ -33,7 +34,44 @@ class SendMessageTest extends TestCase
         $this->assertEquals(true, $message->isCheckSTOPEnabled());
         $this->assertEquals('my.email@address.com', $message->getTxtUsEmail());
 
+        $this->assertTrue($message->hasTxtUsEmail());
         $this->assertTrue($message->hasHourValidity());
         $this->assertTrue($message->hasCustomTag());
+    }
+
+    public function testCreatingObjectWithNoRecipientsThrowsInvalidMessageException()
+    {
+        $this->expectException(InvalidMessageException::class);
+        $this->expectExceptionCode(InvalidMessageException::E_RECIPIENTS_TOO_FEW);
+        new SendMessage(
+            'My test message',
+            [],
+            'Testing',
+            'customtag',
+            50,
+            true,
+            'my.email@address.com'
+        );
+    }
+
+    public function testCreatingObjectWithOver500RecipientsThrowsInvalidMessageException()
+    {
+        $this->expectException(InvalidMessageException::class);
+        $this->expectExceptionCode(InvalidMessageException::E_RECIPIENTS_TOO_MANY);
+
+        $recipients = [];
+
+        for ($i = 0; $i < 501; $i++) {
+            $recipients[] = str_pad((string) $i, 10, '0', STR_PAD_LEFT);
+        }
+        new SendMessage(
+            'My test message',
+            $recipients,
+            'Testing',
+            'customtag',
+            50,
+            true,
+            'my.email@address.com'
+        );
     }
 }
